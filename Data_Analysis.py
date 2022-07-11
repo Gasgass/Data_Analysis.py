0092f976b09521dbb398104bf74904a06de0dbe7
gaston segui <seguigastonmail.com>
2022-07-11 16:42:10 -0300

    Data

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -9,15 +9,8 @@ from selenium.webdriver.common.by import By
 
 
 website = "https://servicios.pami.org.ar/vademecum/views/consultaPublica/listado.zul"
-
 #input_2 = input("ingrese su farmaco: ")
-
-
-
 #WebScraping Vademecum
-
-
-
 #Defino las funciones
 def keys_2():
     WebDriverWait(driver, 5).until(
@@ -29,19 +22,8 @@ def click(XPATH):
         EC.element_to_be_clickable((By.XPATH, XPATH))) \
         .click()
 
-def scrap(XPATH):
-    WebDriverWait(driver, 30).until(EC.element_to_be_clickable(
-        (By.XPATH, XPATH)))
-
-def remove_punc(string):
-    punt = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
-    for ele in string:
-        if ele in punt:
-            string = string.replace(ele, "")
-    return string
-
 #Esta funcion se encarga de tomar todos los datos de los farmacos y almacenarlos en archivos csv
-def scrap_precios():
+def scrap_precios(i):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(10)
     precio = driver.find_elements_by_xpath(
@@ -49,50 +31,49 @@ def scrap_precios():
 
     precios = []
     for a in precio:
-        precios.append(a.text)
+        precios.append(a.text.strip("$"))
     precios = [ele for ele in precios if ele.strip()]
-
-    farmaco = []
-    for n in range(len(precios)):
-        data = {"Precio": precios[n].strip("$"),
-                }
-        farmaco.append(data)
-    df_data = pd.DataFrame(farmaco)
+    df_data = pd.DataFrame(precios)
     print(df_data)
-    df_data.to_csv("a.csv", index=False, header=False, mode="a")
-
+    df_data.to_csv(f"{i}.csv", index=False, header=False, mode="a")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     click(XPATH="/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     #time.sleep(5)
 
+"""with open("C:\\Users\\segui\\PycharmProjects\\Data_Analysis\\Farmacos\\Enfermedades_mentales.csv", "r") as f:
+    csvreader = csv.reader(f)
+    for i in csvreader:
+        print(i)"""
+driver = webdriver.Chrome(executable_path=r"C:\Users\segui\PycharmProjects\pythonProject4\chromedriver.exe")
 
+#driver.get(website)
 
-#element = driver.find_element_by_xpath('//a[@name="zk_comp_99-next"]').get_attribute("disabled")
 with open("C:\\Users\\segui\\PycharmProjects\\Data_Analysis\\Scrap.csv", "r") as f:
+        csvreader = csv.reader(f)
 
 
-    for i in f:
-        print(i)
-        driver = webdriver.Chrome(executable_path=r"C:\Users\segui\PycharmProjects\pythonProject4\chromedriver.exe")
-        driver.get(website)
-        while True:
-
+        for i in csvreader:
+            driver.get(website)
+            print(i)
+            keys_2()
+            click(
+                XPATH="/html/body/div/div/div/div/div[2]/div/div/div[1]/div[2]/div/div[3]/table/tbody[1]/tr[7]/td[3]/div/div/div[1]/button")
             try:
-            # Accedo al sitio de ANMAT
-                keys_2(
-                )
-                click(
-                    XPATH="/html/body/div/div/div/div/div[2]/div/div/div[1]/div[2]/div/div[3]/table/tbody[1]/tr[7]/td[3]/div/div/div[1]/button")
+
                 while True:
-                    scrap_precios()
+                    scrap_precios(i)
                     element = driver.find_element_by_xpath('//a[@name="zk_comp_99-next"]').get_attribute("disabled")
                     if element == None:
                         continue
                     else:
-                        scrap_precios()
+                        scrap_precios(i)
                         time.sleep(10)
                         driver.quit()
-                        break
-                        # quit()
+
             except:
-                break
+                driver.quit()
+                #quit()
+
+
 
