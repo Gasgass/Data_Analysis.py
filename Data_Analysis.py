20e8c26e7c1f755e70d9c27dcbf1484a1c315fba
gaston segui <seguigastonmail.com>
2022-07-13 19:43:20 -0300

    Datas

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -6,10 +6,12 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 website = "https://servicios.pami.org.ar/vademecum/views/consultaPublica/listado.zul"
-#input_2 = input("ingrese su farmaco: ")
+driver = webdriver.Chrome(executable_path=r"C:\Users\segui\PycharmProjects\pythonProject4\chromedriver.exe")
+
 #WebScraping Vademecum
 #Defino las funciones
 def keys_2():
@@ -23,7 +25,7 @@ def click(XPATH):
         .click()
 
 #Esta funcion se encarga de tomar todos los datos de los farmacos y almacenarlos en archivos csv
-def scrap_precios(i):
+def scrap_precios():
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(10)
     precio = driver.find_elements_by_xpath(
@@ -39,41 +41,46 @@ def scrap_precios(i):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     click(XPATH="/html/body/div/div/div/div/div[2]/div/div/div[3]/ul/li[4]/a/i")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    #time.sleep(5)
-
-"""with open("C:\\Users\\segui\\PycharmProjects\\Data_Analysis\\Farmacos\\Enfermedades_mentales.csv", "r") as f:
-    csvreader = csv.reader(f)
-    for i in csvreader:
-        print(i)"""
-driver = webdriver.Chrome(executable_path=r"C:\Users\segui\PycharmProjects\pythonProject4\chromedriver.exe")
-
-#driver.get(website)
 
 with open("C:\\Users\\segui\\PycharmProjects\\Data_Analysis\\Scrap.csv", "r") as f:
-        csvreader = csv.reader(f)
+        a=csv.reader(f)
+        d=[]
 
-
-        for i in csvreader:
-            driver.get(website)
+        for i in a:
             print(i)
+
+            driver.get(website)  # Este driver tiene que ir dentro del loop
             keys_2()
             click(
                 XPATH="/html/body/div/div/div/div/div[2]/div/div/div[1]/div[2]/div/div[3]/table/tbody[1]/tr[7]/td[3]/div/div/div[1]/button")
-            try:
+            print("d")
+            while True:
 
-                while True:
-                    scrap_precios(i)
-                    element = driver.find_element_by_xpath('//a[@name="zk_comp_99-next"]').get_attribute("disabled")
-                    if element == None:
-                        continue
-                    else:
-                        scrap_precios(i)
-                        time.sleep(10)
-                        driver.quit()
+                try:
+                    scrap_precios()
 
-            except:
-                driver.quit()
-                #quit()
+
+                    try:
+                        print("a")
+                        element = driver.find_element_by_xpath('//a[@name="zk_comp_99-next"]').get_attribute("disabled")
+
+                        if element == None:
+                            print("c")
+                            pass
+
+                        else:
+                            print("b")
+                            scrap_precios()
+                            #time.sleep(10)
+                            driver.quit()
+                    except:
+                        quit("Terminó")
+                except:
+                    print("ñ")
+                    break
+
+
+
 
 
 
